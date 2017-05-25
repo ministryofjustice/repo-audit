f2abe81f36df80353f739b9aff5d0b96c7f4bad3
David Salgado <david.salgado@digital.justice.gov.uk>
2017-05-25 15:38:52 +0100

    Allow reporting on a repo by user and name
    Allow Report to be instantiated with a github user
    and a repo name. This enables analysing a single
    repo, given its name, rather than having to
    iterate over all repos owned by a user/org

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -2,11 +2,23 @@ require_relative 'spec_helper'
 
 describe RepoAudit::Report do
   let(:repo) { double(Github::Mash, { full_name: 'some-user/not-a-real-repo' }) }
-  let(:report) { described_class.new(repo) }
+  let(:report) { described_class.new(repo: repo) }
   let(:checker) { double(run: nil) }
 
   before do
     allow(RepoAudit::LicenseChecker).to receive(:new).with(repo).and_return(checker)
+  end
+
+  it 'accepts a repo' do
+    expect { described_class.new(repo: repo) }.to_not raise_error
+  end
+
+  it 'accepts a user & repo name' do
+    expect { described_class.new(user: 'some-user', repo: 'some-repo') }.to_not raise_error
+  end
+
+  it 'raises an exception when required parameters are not supplied' do
+    expect { described_class.new(foo: 'bar') }.to raise_error(KeyError)
   end
 
   it 'reports on a repo' do
@@ -14,6 +26,17 @@ describe RepoAudit::Report do
     expect(checker).to receive(:run)
 
     report.run
+  end
+
+  it 'retrieves a repo given a user & name' do
+    repos = double(find: repo)
+    github = double(repos: repos)
+
+    expect(Github::Client).to receive(:new).and_return(github)
+    expect(github).to receive(:repos).and_return(repos)
+    expect(repos).to receive(:find).with(user: 'some-user', repo: 'some-repo').and_return(repo)
+
+    described_class.new(user: 'some-user', name: 'some-repo').run
   end
 
   it 'returns a report hash' do
