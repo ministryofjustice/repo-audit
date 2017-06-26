89794f6406ce9aca186e875d931fb135b28e86f3
David Salgado <david.salgado@digital.justice.gov.uk>
2017-06-26 12:58:42 +0100

    Update check-repo script wrt. name change

--- bin/check-repo.rb
+++ bin/check-repo.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-require_relative '../lib/repoman'
+require_relative '../lib/repo-audit'
 
 user = ARGV.shift
 name = ARGV.shift
@@ -9,12 +9,12 @@ if (user.to_s.empty? or name.to_s.empty?)
   puts <<~USAGE
   USAGE: #{$0} [github username] [repository name]
 
-    e.g. #{$0} ministryofjustice repoman
+    e.g. #{$0} ministryofjustice repo-audit
 
   USAGE
   exit(1)
 end
 
-ap Repoman::Report.new(user: user, name: name).run
+ap RepoAudit::Report.new(user: user, name: name).run
 puts
 
