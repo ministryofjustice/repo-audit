b86cace13f2e1000077e77e9c4586f5ce549a3d3
David Salgado <david.salgado@digital.justice.gov.uk>
2017-07-20 10:55:38 +0100

    Alter the analysis script so it outputs valid JSON
    The intention is to fee the JSON output through whatever transform
    is required to generate the appropriate output format for the
    results. e.g. output the results as a CSV, suitable for turning
    into a spreadsheet

--- bin/analyse.rb
+++ bin/analyse.rb
@@ -3,7 +3,8 @@
 require_relative '../lib/repo-audit'
 
 # Just check a few repos, for speed
-RepoAudit::RepositoryHelper.list(user: 'ministryofjustice')[10..15].each do |repo|
-  ap RepoAudit::Report.new(repo: repo).run
-  puts
+results = RepoAudit::RepositoryHelper.list(user: 'ministryofjustice')[10..15].inject([]) do |list, repo|
+  list << RepoAudit::Report.new(repo: repo).run
 end
+
+puts results.to_json
