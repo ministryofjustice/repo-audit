8bdec4480ef0b8372cba4305db3a5823fd2000da
David Salgado <david.salgado@digital.justice.gov.uk>
2017-07-21 13:09:17 +0100

    Implement the last_commit_check
    Outputs the timestamp, author name and author
    email of the last commit to the repo, in the
    results hash.

--- lib/repo-audit/checks/last_commit_check.rb
+++ lib/repo-audit/checks/last_commit_check.rb
@@ -1,6 +1,18 @@
 module RepoAudit::Checks
   class LastCommitCheck < BaseCheck
     register_check :last_commit_check
+
+    def run(repo)
+      commit = RepoAudit::RepositoryHelper.last_commit(user: repo.owner.login, name: repo.name)
+      author = commit.fetch("commit").fetch("author")
+      result(
+        timestamp: DateTime.parse(author.fetch("date")),
+        author: {
+          name: author.fetch("name"),
+          email: author.fetch("email")
+        }
+      )
+    end
   end
 end
 
