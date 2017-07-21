6766bddb0cbe52f5578dafdfc62637d6de07a54a
Jesus Laiz <zheileman@users.noreply.github.com>
2017-07-21 10:00:32 +0100

    Extract shared tests from check specs
    Some refactor of the check tests as now we are going to have identical
    tests for some of the code in each of the checks, so we can make use of
    shared examples to tidy up this a little bit.
    
    These shared tests cover the common areas of every check (initialization,
    metadata, arguments...) so the individual check specs can focus on the
    `#run` method which is different for each check.

--- spec/checks/required_file_check_spec.rb
+++ spec/checks/required_file_check_spec.rb
@@ -6,61 +6,7 @@ describe RepoAudit::Checks::RequiredFileCheck do
 
   subject { described_class.new(metadata, arguments) }
 
-  it 'registered itself in the factory store' do
-    expect(RepoAudit::ChecksFactory.fetch(:required_file_check)).to eq(described_class)
-  end
-
-  describe '.new' do
-    it 'requires the metadata and the arguments' do
-      expect { described_class.new }.to raise_error(ArgumentError, /given 0, expected 2/)
-    end
-
-    context 'configures the check with the metadata' do
-      context '`description` declared in the metadata' do
-        it 'has a description' do
-          expect(subject.description).to eq('check description')
-        end
-      end
-
-      context '`description` not declared in the metadata' do
-        let(:metadata) { super().except(:description) }
-
-        it 'raises an error' do
-          expect { subject.description }.to raise_error(KeyError, /description/)
-        end
-      end
-
-      context 'style_guide_url declared in the metadata' do
-        it 'has a style_guide_url' do
-          expect(subject.style_guide_url).to eq('www.example.com')
-        end
-      end
-
-      context 'style_guide_url not declared in the metadata' do
-        let(:metadata) { super().except(:style_guide_url) }
-
-        it 'has a default style_guide_url value' do
-          expect(subject.style_guide_url).to eq('n/a')
-        end
-      end
-    end
-
-    context 'configures the check with the arguments' do
-      context 'when expected arguments are provided' do
-        it 'sets the `name_matcher` value' do
-          expect(subject.filename).to eq('document.txt')
-        end
-      end
-
-      context 'when unexpected arguments are provided' do
-        let(:arguments) { {whatever: 'hello'} }
-
-        it 'raises an exception' do
-          expect { subject.name_matcher }.to raise_error(NoMethodError, /whatever/)
-        end
-      end
-    end
-  end
+  it_behaves_like 'a Check', name: :required_file_check
 
   context '#run' do
     let(:repository) { double('Repository', full_name: 'org/repo-name') }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'checks/checks_shared_examples'
 require_relative '../lib/repo-audit'
 
 RSpec.configure do |config|
